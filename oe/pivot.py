8234bf8fda8b9b478aebf9f8302058940ef2d4d8
gpiotti <gonzalopiotti@hotmail.com>
2017-03-20 16:50:44 -0300

    modified pivot.py

--- oe/pivot.py
+++ oe/pivot.py
@@ -1,39 +1,45 @@
+#get the model output and pivots columns for easy excel visualization
+#input assignment.csv
+#output pivot.csv
 from collections import OrderedDict
 import csv
-myfile = open('pivot.csv', 'w')
+myfile = open('pivoted_output.csv', 'w')
 
 def readCsv(path):
     with open(path, 'r') as f:
-        reader = csv.reader(f)
+        reader = csv.reader(f,delimiter=';')
         return list(reader)
 
-
+shiftNames = ["a3731000000573uAAA","a3731000000573zAAA","a3731000000573kAAA","off"]
 temp = OrderedDict()
 
 csv_input = readCsv('assignment.csv')
-  
+#print (csv_input)
 for row in csv_input:
-    teacher = row[0]
-    temp[teacher] = []
+    if row[0] != '----------' and row[0] != '==========':
+        teacher = str(row[1])
+        temp[teacher] = []
+
 
 assignment = []
 for row in csv_input:
-    if str(row[2]) == 'LP2 - Classic Intermediate':
-        i = 'INT'
-    elif str(row[2]) == 'LP2 - Classic True Beginner - Portuguese':
-        i = 'TBP'
-    elif str(row[2]) == 'LP2 - Monitor':
-        i = 'MON'
-    else:
-        i = 'off'
-    temp[row[0]].append(i)
+    if row[0] != '----------' and row[0] != '==========':
+        if str(row[2]) == shiftNames[0]:
+            i = 'INT'
+        elif str(row[2]) == shiftNames[1]:
+            i = 'TBP'
+        elif str(row[2]) == shiftNames[2]:
+            i = 'MON'
+        else:
+            i = shiftNames[3]
+        temp[row[1]].append(i)
 
-        
+   
 
 text = ''
 for teacher in temp:
     text = text +  teacher + ',' + (','.join(temp[teacher])) + '\n'
 
 myfile.write(text)
-
+myfile.close()
 
